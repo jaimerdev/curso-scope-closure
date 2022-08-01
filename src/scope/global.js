7e504e58b9924f6bb49d8abead04b5b34c18f5d3
Jaime Guerrero <jaime97guerrero@gmail.com>
2022-08-01 13:19:20 -0500

    Añadí la carpeta 'src' a los elementos rastreados

---
+++ src/scope/global.js
@@ -0,0 +1,21 @@
+//variables 
+var a; //declarando
+var b = 'b';//declaramos / asignamos
+b = 'bb'; //reasignación
+var a = 'aa'; //redeclaración
+
+// Global Scope
+var fruit = 'Apple'; //global
+
+function bestFruit(){
+    console.log(fruit)
+}
+bestFruit();
+
+function countries(){
+    country = 'Ecuador'; //global
+    console.log(country);
+}
+
+countries();
+console.log(country);
