c0469babcd1436dbfef2e3d825bf772a8b0f6827
Jaime Guerrero <jaime97guerrero@gmail.com>
2022-08-02 12:29:17 -0500

    Cuarta clase, Redeclarar y Reasignar

---
+++ src/scope/re.js
@@ -0,0 +1,24 @@
+var firstName; // Undefined
+firstName = 'Jaime';
+console.log(firstName);
+
+var lastName = 'Guerrero';// declarar / asignar
+lastName = 'Reyes';// reasignar 
+console.log(lastName);
+
+var secondName = 'Jaime';// declarar / asignar 
+var secondName = 'Lenin';// reasignando
+console.log(secondName);
+
+//Let 
+let fruit = 'Apple' // declarar y asignar 
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+//Const 
+const animal = 'dog'; // declarar y asignar
+animal = 'cat'; //reasignar 
+console.log(animal);
